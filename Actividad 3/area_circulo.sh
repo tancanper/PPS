072864cfb4bd1bcb9f48b5a6b79d0525c63b4abe
tancanper <tancanper@alu.edu.gva.es>
2024-11-03 23:25:38 +0100

    Add files via upload

---
+++ Actividad 3/area_circulo.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+#Calculo del área de un círculo
+
+read -p "Introduce el radio del círculo: " radio
+
+#Calcular el área del círculo
+area=$(echo "3.1416 * $radio * $radio" | bc)
+
+echo "El área del círculo es: $area"
